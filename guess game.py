567e83b16173579d39657e002a4ce3ec2837ce1a
Elking <seifelsayed68@gmail.com>
2023-05-29 20:54:52 +0300

    add guess game

---
+++ guess game.py
@@ -0,0 +1,40 @@
+import random
+
+print("Welcome to the guess game")
+
+def random_num(length):
+    num = ""
+    char = "0123456789"
+    for _ in range(length):
+        num += random.choice(char)
+    return num
+
+length = 1  # تحديد طول العدد العشوائي
+num = random_num(length)
+
+count = 0
+limit = 3
+lose = False
+
+while True:
+    if count < limit:
+        ask = input("Guess the number randomly chosen by the computer from zero to nine: ")
+        count += 1
+
+        if ask == num:
+            print("Congratulations! You guessed the correct number.")
+            break
+        else:
+            print("Wrong guess. Try again.")
+    else:
+        lose = True
+        break
+
+if lose:
+    print("You reached the maximum number of attempts. The correct number was:", num)
+
+
+
+
+
+    
